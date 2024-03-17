709c35fa6dce37b9d2dd2ed7874d78e8733d3101
Simarpal Singh <simarpalsingh03@gmail.com>
2024-03-17 14:02:25 -0700

    changed spaceguy path

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '/Users/simar/Documents/projectportfolio/src/assets/img/spaceguy.png';
+import logo from '../assets/img/spaceguy.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
